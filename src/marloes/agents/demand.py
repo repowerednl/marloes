58b152184beb8f76de4b3407cc13934d7610d76e
Matthiasd23 <matthiasdrijfhout23@gmail.com>
2024-11-19 16:53:46 +0100

    test demand init

--- src/marloes/agents/demand.py
+++ src/marloes/agents/demand.py
@@ -1,10 +1,13 @@
 """Demand 'agents' with functionality from Repowered's Simon"""
+from datetime import datetime
 from simon.assets.demand import Demand
 from .base import Agent
 
+
 class DemandAgent(Agent):
-    def __init__(self, config: dict):
-        model = Demand(config)
+    def __init__(self, start_time: datetime, config: dict):
+        model = Demand(**config)
+        model.load_default_state(time=start_time)
 
         super().__init__(model)
 
