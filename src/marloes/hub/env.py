5473814842a07aa7bd9dbc98707836ce633e1d40
Matthiasd23 <matthiasdrijfhout23@gmail.com>
2024-11-19 15:33:38 +0100

    env step

--- src/marloes/hub/env.py
+++ src/marloes/hub/env.py
@@ -10,8 +10,7 @@ from agents.wind import WindAgent
 class EnergyHub:
     def __init__(self, config: dict):
         self.algorithm = config['algorithm']
-        for agent_config in config['agents']:
-            self.add_agent(agent_config)
+        [self.add_agent(agent_config) for agent_config in config['agents']] 
         # handle other config parameters
 
     def add_agent(self, config: dict):
@@ -27,7 +26,26 @@ class EnergyHub:
             agent = WindAgent(config)
         # add to self.agents with necessary priorities, edges or constraints
     
-    def train(self):
-        # train the agents
+    def _combine_states(self):
+        """ Function to combine all agents states into one observation """
         pass
+
+    def _calculate_reward(self):
+        """ Function to calculate the reward """
+        pass
+
+    def step(self):
+        """ Function should return the observation, reward, done, info """
+        # step the agents
+        [ agent.act() for agent in self.agents ] # function is called act now, can be renamed
+        # gather observations
+        # either combine every agents state into one observation or a list of observations for each agent
+        observation = self._combine_states()
+        # calculate reward (CO2 emission factor, sum of taking/feeding in, current net balance)
+        reward = self._calculate_reward()
+        # is the hub ever done? Is life ever done? Is life a simulation?
+        done = False
+        # info for debugging purposes
+        info = {}
+        return observation, reward, done, info
 
