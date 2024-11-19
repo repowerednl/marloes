9f6524f02156b60e7c54a7e920c2522e9072ee08
Matthiasd23 <matthiasdrijfhout23@gmail.com>
2024-11-19 16:16:20 +0100

    initialization test passes

--- test/agents/test_battery.py
+++ test/agents/test_battery.py
@@ -1,5 +1,8 @@
 import unittest
+from freezegun import freeze_time
+from datetime import datetime, timedelta
 from simon.assets.battery import Battery
+from simon.data.battery_data import BatteryState
 from marloes.agents.battery import BatteryAgent
 
 CONFIG = {
@@ -16,18 +19,27 @@ CONFIG = {
 }
 
 class TestBatteryAgent(unittest.TestCase):
+    @freeze_time("2023-01-01 12:00:00")
+    def setUp(self) -> None:
+        self.battery_agent = BatteryAgent(CONFIG)
+        self.initial_state = BatteryState(
+            time=datetime.now(),
+            power=-20,
+            state_of_charge=0.5,
+            degradation=0.0
+        )
+        self.battery_agent.model.set_state(self.initial_state)
 
     def test_init(self):
-        battery_agent = BatteryAgent(CONFIG)
-        self.assertIsInstance(battery_agent.model, Battery)
-        self.assertEqual(battery_agent.model.name, "BatteryOne")
-        self.assertEqual(battery_agent.model.max_power_in, 50.0)
-        self.assertEqual(battery_agent.model.max_power_out, 50.0)
-        self.assertEqual(battery_agent.model.max_state_of_charge, 1.0)
-        self.assertEqual(battery_agent.model.min_state_of_charge, 0.0)
-        self.assertEqual(battery_agent.model.energy_capacity, 100.0)
-        self.assertEqual(battery_agent.model.ramp_up_rate, 10.0)
-        self.assertEqual(battery_agent.model.ramp_down_rate, 10.0)
-        self.assertEqual(battery_agent.model.input_efficiency, 0.95)
-        self.assertEqual(battery_agent.model.output_efficiency, 0.95)
-        self.assertIsNone(battery_agent.model.degradation_function)
+        self.assertIsInstance(self.battery_agent.model, Battery)
+        self.assertEqual(self.battery_agent.model.name, "BatteryOne")
+        self.assertEqual(self.battery_agent.model.max_power_in, 50.0)
+        self.assertEqual(self.battery_agent.model.max_power_out, 50.0)
+        self.assertEqual(self.battery_agent.model.max_state_of_charge, 1.0)
+        self.assertEqual(self.battery_agent.model.min_state_of_charge, 0.0)
+        self.assertEqual(self.battery_agent.model.energy_capacity, 100.0)
+        self.assertEqual(self.battery_agent.model.ramp_up_rate, 10.0)
+        self.assertEqual(self.battery_agent.model.ramp_down_rate, 10.0)
+        self.assertEqual(self.battery_agent.model.input_efficiency, 0.9**0.5)
+        self.assertEqual(self.battery_agent.model.output_efficiency, 0.9**0.5)
+        self.assertIsNone(self.battery_agent.model.degradation_function)
