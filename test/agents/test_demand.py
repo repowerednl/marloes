ee143b22a1c37444b61d0c66c2fc021ceaf33a19
Lucas Velvis <lucasvelvis@gmail.com>
2024-11-27 16:26:41 +0100

    Some more test adjustments

--- test/agents/test_demand.py
+++ test/agents/test_demand.py
@@ -20,21 +20,19 @@ CONFIG = {
 
 @freeze_time("2023-01-01 12:00:00")
 class TestDemandAgent(unittest.TestCase):
-    def setUp(self) -> None:
-        self.demand_agent = DemandAgent(start_time=datetime.now(), config=CONFIG)
-
     def test_init(self):
-        self.assertIsInstance(self.demand_agent.asset, Demand)
-        self.assertEqual(self.demand_agent.asset.name, "DemandOne")
-        self.assertEqual(self.demand_agent.asset.max_power_in, 10.0)
-        self.assertIsInstance(self.demand_agent.asset.data_source, DummyDataSource)
-        self.assertEqual(self.demand_agent.asset.data_source.value, 5.0)
+        demand_agent = DemandAgent(start_time=datetime.now(), config=CONFIG)
+        self.assertIsInstance(demand_agent.asset, Demand)
+        self.assertEqual(demand_agent.asset.name, "DemandOne")
+        self.assertEqual(demand_agent.asset.max_power_in, 10.0)
+        self.assertIsInstance(demand_agent.asset.data_source, DummyDataSource)
+        self.assertEqual(demand_agent.asset.data_source.value, 5.0)
 
     def test_partial_init(self):
-        partial_config = {}
-        self.demand_agent = DemandAgent(
-            start_time=datetime.now(), config=partial_config
-        )
-        self.assertIsInstance(self.demand_agent.asset, Demand)
-        self.assertEqual(self.demand_agent.asset.name, "Demand")
-        self.assertFalse(self.demand_agent.asset.curtailable_by_solver)
+        partial_config = {
+            "profile": "Farm",
+        }
+        demand_agent = DemandAgent(start_time=datetime.now(), config=partial_config)
+        self.assertIsInstance(demand_agent.asset, Demand)
+        self.assertEqual(demand_agent.asset.name, "Demand")
+        self.assertFalse(demand_agent.asset.curtailable_by_solver)
