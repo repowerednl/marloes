8d2b75a04b9079acd782a7539e358dc52c6e836a
Lucas Velvis <lucasvelvis@gmail.com>
2024-11-27 13:40:16 +0100

    Adjust validate config for Non-DRL algorithm

--- src/marloes/validation/validate_config.py
+++ src/marloes/validation/validate_config.py
@@ -1,24 +1,33 @@
 # imports
 
+
 def validate_config(config: dict) -> str:
     """Validate the configuration dictionary."""
-    # Check if all required keys are present
-    required_keys = ["algorithm", "epochs", "learning_rate" if not config["grid_search"] else "coverage"]
-    for key in required_keys:
-        if key not in config:
-            return f"Error: Missing key '{key}' in configuration"
-    
     # Check if the algorithm is valid
-    valid_algorithms = ["model_based", "model_free"]
-    if config["algorithm"] not in valid_algorithms:
-        return f"Error: Invalid algorithm '{config['algorithm']}'"
-    
-    # Check if epochs is a positive integer
-    if not isinstance(config["epochs"], int) or config["epochs"] <= 0:
-        return "Error: 'epochs' must be a positive integer"
-    
-    # Check if learning_rate is a positive float
-    if not config["grid_search"] and ( not isinstance(config["learning_rate"], float) or config["learning_rate"] <= 0 ):
-        return "Error: 'learning_rate' must be a positive float"
-    
+    valid_algorithms = ["model_based", "model_free", "solver"]
+    algorithm = config.get("algorithm", None)
+    if algorithm not in valid_algorithms:
+        return f"Error: Invalid algorithm '{algorithm}'"
+
+    if config["algorithm"] in ["model_based", "model_free"]:
+        # Check if required keys are present
+        required_keys = [
+            "epochs",
+            "learning_rate" if not config["grid_search"] else "coverage",
+        ]
+        for key in required_keys:
+            if key not in config:
+                return f"Error: Missing key '{key}' in configuration"
+
+        # Check if epochs is a positive integer
+        if not isinstance(config["epochs"], int) or config["epochs"] <= 0:
+            return "Error: 'epochs' must be a positive integer"
+
+        # Check if learning_rate is a positive float
+        if not config["grid_search"] and (
+            not isinstance(config["learning_rate"], float)
+            or config["learning_rate"] <= 0
+        ):
+            return "Error: 'learning_rate' must be a positive float"
+
     return "Configuration is valid"
