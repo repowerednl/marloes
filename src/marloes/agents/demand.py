3562114cd598f62c47b620ec35e1d97c90eef8bd
Lucas Velvis <lucasvelvis@gmail.com>
2024-11-27 15:07:53 +0100

    Update testing + Demand default config

--- src/marloes/agents/demand.py
+++ src/marloes/agents/demand.py
@@ -2,16 +2,27 @@
 from datetime import datetime
 from simon.assets.demand import Demand
 from .base import Agent
+import numpy as np
+import pandas as pd
 
 
 class DemandAgent(Agent):
     def __init__(self, config: dict, start_time: datetime):
-        series = self.get_demand_series(config)
+        series = self._get_demand_series(config)
         super().__init__(Demand, config, start_time, series)
 
-    def get_demand_series(self, config: dict):
+    def _get_demand_series(self, config: dict):
         # TODO: to be implemented
-        pass
+        return pd.Series()
+
+    def get_default_config(cls, config: dict) -> dict:
+        """Each subclass must define its default configuration."""
+        return {
+            "name": "Demand",
+            "max_power_in": np.inf,
+            # Should not be curtailed
+            "curtailable_by_solver": False,
+        }
 
     def act(self):
         pass
