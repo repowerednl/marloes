f4d9e8833eae2bbfc72f67ed6a80ed9c585a91c7
Lucas Velvis <lucasvelvis@gmail.com>
2024-12-10 15:03:37 +0100

    Simplify init

--- src/marloes/results/extractor.py
+++ src/marloes/results/extractor.py
@@ -13,24 +13,23 @@ class Extractor:
     def __init__(self, chunk_size: int = 0):
         # Position tracking
         self.i = 0
+        size = MINUTES_IN_A_YEAR // chunk_size
 
         # Marl(oes) info
-        self.elapsed_time = np.zeros(MINUTES_IN_A_YEAR // chunk_size)
-        self.loss = np.zeros(MINUTES_IN_A_YEAR // chunk_size)
-        self.action_probability_distribution = np.zeros(
-            (MINUTES_IN_A_YEAR // chunk_size, 2)
-        )
+        self.elapsed_time = np.zeros(size)
+        self.loss = np.zeros(size)
+        self.action_probability_distribution = np.zeros((size, 2))
 
         # Metrics/Reward info
-        self.grid_to_demand = np.zeros(MINUTES_IN_A_YEAR // chunk_size)
-        self.demand_state = np.zeros(MINUTES_IN_A_YEAR // chunk_size)
-        self.grid_state = np.zeros(MINUTES_IN_A_YEAR // chunk_size)
+        self.grid_to_demand = np.zeros(size)
+        self.demand_state = np.zeros(size)
+        self.grid_state = np.zeros(size)
 
         # Emmission info
-        self.total_solar_production = np.zeros(MINUTES_IN_A_YEAR // chunk_size)
-        self.total_battery_production = np.zeros(MINUTES_IN_A_YEAR // chunk_size)
-        self.total_wind_production = np.zeros(MINUTES_IN_A_YEAR // chunk_size)
-        self.total_grid_production = np.zeros(MINUTES_IN_A_YEAR // chunk_size)
+        self.total_solar_production = np.zeros(size)
+        self.total_battery_production = np.zeros(size)
+        self.total_wind_production = np.zeros(size)
+        self.total_grid_production = np.zeros(size)
 
     def clear(self):
         """Clear the extractor"""
