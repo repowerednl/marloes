c2c3b802c01a7996e2bf10dad3c2af7d4d11d19a
Matthiasd23 <matthiasdrijfhout23@gmail.com>
2024-12-19 14:20:14 +0100

    init extractor

--- src/marloes/results/calculator.py
+++ src/marloes/results/calculator.py
@@ -1,35 +1,38 @@
 from .extractor import Extractor
 
+# from marloes.valley.rewards import Reward
+
 
 class Calculator:
-    def __init__(self):
-        self.extractor = Extractor()
+    def __init__(self, uid: int | None = None, dir: str = "results"):
+        # also initialize the Extractor
+        self.extractor = Extractor(from_model=False)
+        # and read the necessary information from files
+        self.extractor.from_files(uid, dir)
+        # information can be accessed through the extractor attributes
 
-    def _get_marloes_info(self):
+    def _get_metrics(self, metrics: list[str]) -> list:
         """
-        Function to get the necessary information for the marloes using the Extractor
+        Function to get the necessary information (metrics/attributes from the extractor)
         """
-        all_marloes = {
-            "marloes": "marloes",
-            "marloes2": "marloes2",
-        }
-        marloes = {}
-        for key, _ in all_marloes.items():
-            series = self.extractor.from_file(key)
-            marloes[key] = series
+        # initialize the list to store the metrics
+        metric_values = []
+        for metric in metrics:
+            attribute = self._match_metric_to_attribute(metric)
+            if not attribute:
+                metric_values.append(None)
+                continue
+            metric_values.append(getattr(self.extractor, attribute))
 
-    def _get_metric_info(self):
+    def _match_metric_to_attribute(self, metric: str) -> str | None:
         """
-        Function to get the necessary information for the metrics using the Extractor
+        Function to match the metric to the corresponding attribute in the extractor
         """
-        all_metrics = {
-            "metric": "metric",
-            "metric2": "metric2",
-        }
-        metrics = {}
-        for key, _ in all_metrics.items():
-            series = self.extractor.from_file(key)
-            metrics[key] = series
+        # check if the metric is in the extractor attributes
+        if hasattr(self.extractor, metric):
+            return metric
+        # if not, return None
+        return None
 
     def calculate(self):
         return self.extractor.extract() + 1
