e35572f4b5424163c35491b1cbad009448fca7c4
Matthiasd23 <matthiasdrijfhout23@gmail.com>
2025-02-05 13:58:55 +0100

    basic networkconfig + test

---
+++ src/marloes/networks/configuration.py
@@ -0,0 +1,66 @@
+import os
+import json
+
+
+class NetworkConfig:
+    """
+    A class to manage network configurations, allowing it to be used as a dictionary
+    and providing functionality to load and save configurations from/to a file.
+    Attributes:
+        networks (dict): A dictionary to store network configurations.
+    Methods:
+        __getitem__(key):
+            Returns the value associated with the given key in the networks dictionary.
+        __setitem__(key, value):
+            Sets the value for the given key in the networks dictionary.
+        __delitem__(key):
+            Deletes the key-value pair associated with the given key from the networks dictionary.
+        __iter__():
+            Returns an iterator over the keys of the networks dictionary.
+        __len__():
+            Returns the number of key-value pairs in the networks dictionary.
+        load(uid):
+            Loads the network configuration from a file identified by the given UID.
+        save(uid):
+            Saves the current network configuration to a file identified by the given UID.
+    """
+
+    def __init__(self):
+        self.networks = {}
+
+    """
+    Below are the load and save functions that allow the configuration to be saved and loaded from a file.
+    """
+
+    def load(self, uid):
+        path = f"results/network/config/{uid}.json"
+        if os.path.exists(path):
+            with open(path, "r") as f:
+                self.networks = json.load(f)
+        else:
+            raise FileNotFoundError(f"No configuration found for UID: {uid}")
+
+    def save(self, uid):
+        path = f"results/network/config/{uid}.json"
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, "w") as f:
+            json.dump(self.networks, f)
+
+    """
+    Below is functionality that allows the NetworkConfig class to be used as a dictionary.
+    """
+
+    def __getitem__(self, key):
+        return self.networks[key]
+
+    def __setitem__(self, key, value):
+        self.networks[key] = value
+
+    def __delitem__(self, key):
+        del self.networks[key]
+
+    def __iter__(self):
+        return iter(self.networks)
+
+    def __len__(self):
+        return len(self.networks)
---
+++ test/networks/test_configuration.py
@@ -0,0 +1,45 @@
+import unittest
+import os
+import json
+from marloes.networks.configuration import NetworkConfig
+
+
+class TestNetworkConfig(unittest.TestCase):
+    def setUp(self):
+        self.config = NetworkConfig()
+        self.test_uid = "test_uid"
+        self.test_path = f"results/network/config/{self.test_uid}.json"
+        self.test_data = {"network1": "config1", "network2": "config2"}
+
+    def tearDown(self):
+        if os.path.exists(self.test_path):
+            os.remove(self.test_path)
+
+    def test_set_and_get_item(self):
+        self.config["network1"] = "config1"
+        self.assertEqual(self.config["network1"], "config1")
+
+    def test_del_item(self):
+        self.config["network1"] = "config1"
+        del self.config["network1"]
+        with self.assertRaises(KeyError):
+            _ = self.config["network1"]
+
+    def test_len(self):
+        self.config["network1"] = "config1"
+        self.config["network2"] = "config2"
+        self.assertEqual(len(self.config), 2)
+
+    def test_load(self):
+        os.makedirs(os.path.dirname(self.test_path), exist_ok=True)
+        with open(self.test_path, "w") as f:
+            json.dump(self.test_data, f)
+        self.config.load(self.test_uid)
+        self.assertEqual(self.config.networks, self.test_data)
+
+    def test_save(self):
+        self.config.networks = self.test_data
+        self.config.save(self.test_uid)
+        with open(self.test_path, "r") as f:
+            data = json.load(f)
+        self.assertEqual(data, self.test_data)
