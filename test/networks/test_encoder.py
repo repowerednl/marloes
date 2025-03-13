e99513b08da69ebd19053a8fc1418b04c2e37b87
Matthiasd23 <matthiasdrijfhout23@gmail.com>
2025-03-13 14:46:34 +0100

    testing util function

---
+++ test/networks/test_encoder.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+import numpy as np
+
+from marloes.networks.WorldModel import Encoder, Decoder, RewardPredictor
+from marloes.networks.details import RSSM_LD
+from marloes.networks.util import observation_to_tensor
+
+
+class EncoderTestCase(TestCase):
+    """
+    Test Case for the Encoder, to test if creation is flexible and forward pass goes through all parts.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        state = {"nom": 1, "test": 2}
+        state_2 = {"nom": 3, "test": 4, "extra": 5}
+        observations = {"agent1": state, "agent2": state_2}
+        tensor = observation_to_tensor(observations, concatenate_all=True)
+        cls.encoder = Encoder(tensor.shape, latent_dim=3)
+
+    def test_encoder_creation(self):
+        """
+        Test if the Encoder is created correctly.
+        """
+        self.assertEqual(self.encoder.fc1.in_features, self.observation_shape[0])
+        self.assertEqual(self.encoder.fc2.out_features, self.latent_dim)
