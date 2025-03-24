ee7c1ed46da4080bbc75a53cc5878a2414623ada
Matthiasd23 <matthiasdrijfhout23@gmail.com>
2025-03-24 16:38:03 +0100

    extensive testing recursive

--- test/networks/test_networks_util.py
+++ test/networks/test_networks_util.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import torch
+import numpy as np
 
 from marloes.networks.util import dict_to_tens
 
@@ -14,6 +15,12 @@ class TestUtil(TestCase):
         cls.observation = {
             "agent1": {"nom": 1, "test": 2},
             "agent2": {"nom": 3, "test": 4, "extra": 5},
+            "agent3": {"nom": np.array([1, 2, 3, 4]), "test": 7},
+            "agent4": {
+                "nom": np.array([1, 2, 3, 4]),
+                "test": 7,
+                "extradict": {"scalar": 1, "vector": np.array([1, 2, 3])},
+            },
         }
         cls.reward = {
             "agent1": 1,
@@ -33,10 +40,15 @@ class TestUtil(TestCase):
         tensor = dict_to_tens(data=self.observation, concatenate_all=True)
         # should be a tensor object
         self.assertIsInstance(tensor, torch.Tensor)
-        # should have shape (5,)
-        self.assertEqual(tensor.shape, torch.Size([5]))
+        # should have shape agent1: 2, agent2: 3, agent3: 4+1, agent4: 4+1+(1+3) = (19,)
+        self.assertEqual(tensor.shape, torch.Size([19]))
         # should be [1, 2, 3, 4, 5]
-        self.assertTrue(torch.equal(tensor, torch.tensor([1, 2, 3, 4, 5])))
+        self.assertTrue(
+            torch.equal(
+                tensor,
+                torch.tensor([1, 2, 3, 4, 5, 1, 2, 3, 4, 7, 1, 2, 3, 4, 7, 1, 1, 2, 3]),
+            )
+        )
 
     def test_observation_to_tensor_separate(self):
         """
@@ -53,8 +65,14 @@ class TestUtil(TestCase):
         self.assertTrue(torch.equal(tensor[0], torch.tensor([1, 2])))
         # second element should be [3, 4, 5]
         self.assertTrue(torch.equal(tensor[1], torch.tensor([3, 4, 5])))
+        # third element should be [1, 2, 3, 4, 7]
+        self.assertTrue(torch.equal(tensor[2], torch.tensor([1, 2, 3, 4, 7])))
+        # fourth element should be [1, 2, 3, 4, 7, 1, 1, 2, 3]
+        self.assertTrue(
+            torch.equal(tensor[3], torch.tensor([1, 2, 3, 4, 7, 1, 1, 2, 3]))
+        )
         # number of agents should be 2
-        self.assertEqual(len(tensor), 2)
+        self.assertEqual(len(tensor), 4)
 
     def test_reward_to_tensor(self):
         """
