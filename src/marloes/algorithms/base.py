c4e14e9fd166422d4c2451e01d29ca6e64a43bad
Matthiasd23 <matthiasdrijfhout23@gmail.com>
2025-03-24 15:02:33 +0100

    commented unnecessary prod code

--- src/marloes/algorithms/base.py
+++ src/marloes/algorithms/base.py
@@ -40,28 +40,28 @@ class BaseAlgorithm(ABC):
         """
         logging.info("Starting training process...")
         observations, infos = self.environment.reset()
-        logging.info("Initializing ReplayBuffer...")
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        capacity = 10000
-        RB = ReplayBuffer(capacity=capacity, device=device)
+        # logging.info("Initializing ReplayBuffer...")
+        # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # capacity = 10000
+        # RB = ReplayBuffer(capacity=capacity, device=device)
 
         for epoch in range(self.epochs):
             if epoch % 1000 == 0:
                 logging.info(f"Reached epoch {epoch}/{self.epochs}...")
 
-            obs = dict_to_tens(observations, concatenate_all=True)
+            # obs = dict_to_tens(observations, concatenate_all=True)
             # Get actions
             actions = self.get_actions(observations)
             # Step in the Environment
             observations, rewards, dones, infos = self.environment.step(actions)
             # Add to ReplayBuffer
-            acts = dict_to_tens(actions, concatenate_all=True)
-            rew = dict_to_tens(rewards, concatenate_all=True)
-            rew = torch.tensor([rew.sum()])
-            next_obs = dict_to_tens(observations, concatenate_all=True)
-            dones = dict_to_tens(dones, concatenate_all=True)
+            # acts = dict_to_tens(actions, concatenate_all=True)
+            # rew = dict_to_tens(rewards, concatenate_all=True)
+            # rew = torch.tensor([rew.sum()])
+            # next_obs = dict_to_tens(observations, concatenate_all=True)
+            # dones = dict_to_tens(dones, concatenate_all=True)
 
-            RB.push(obs, acts, rew, next_obs, dones)
+            # RB.push(obs, acts, rew, next_obs, dones)
 
             # For x timesteps, perform the training step on a sample from the ReplayBuffer
             self._train_step(observations, rewards, dones, infos)
