67567e2d9379737daecbcbbb4e7ca5019b408641
Lucas Velvis <lucasvelvis@gmail.com>
2025-04-09 14:09:50 +0200

    Make base use config

--- src/marloes/networks/SAC/base.py
+++ src/marloes/networks/SAC/base.py
@@ -8,12 +8,16 @@ class SACBaseNetwork(nn.Module):
     It builds the hidden layers for the networks, as those are the same for all networks.
     """
 
-    def __init__(self, input_dim, hidden_dim=256, num_layers=2, activation=nn.ReLU()):
+    def __init__(self, input_dim, config, activation=nn.ReLU()):
         """
         Initializes the SACBaseNetwork with the given input and output dimensions and hidden layer dimensions.
         All provided defaults are based on the original SAC paper.
         """
         super(SACBaseNetwork, self).__init__()
+
+        # Get the parameters from the config
+        hidden_dim = config.get("hidden_dim", 256)
+        num_layers = config.get("num_layers", 2)
 
         # Build the layers
         layers = []
