71c3c1540c77be21fa5dbe19a055092358d6cc2c
Matthiasd23 <matthiasdrijfhout23@gmail.com>
2025-04-14 14:45:58 +0200

    normalizing with returns

--- src/marloes/networks/dreamer/ActorCritic.py
+++ src/marloes/networks/dreamer/ActorCritic.py
@@ -58,7 +58,7 @@ class ActorCritic:
         returns, advantages = self._compute_advantages(rewards, values)
 
         # Compute the actor and critic losses
-        actor_loss = self._compute_actor_loss(states, actions, advantages)
+        actor_loss = self._compute_actor_loss(states, actions, advantages, returns)
         critic_loss = self._compute_critic_loss(values, returns)
 
         # Backpropagate the losses
@@ -80,7 +80,7 @@ class ActorCritic:
             "total_loss": total_loss,
         }
 
-    def _compute_actor_loss(self, states, actions, advantages) -> torch.Tensor:
+    def _compute_actor_loss(self, states, actions, advantages, returns) -> torch.Tensor:
         """
         Computes the actor loss.
         Term 1: policy gradient: the negative log probability of the taken actions, weighted by the advantage.
@@ -91,13 +91,17 @@ class ActorCritic:
 
         entropy = policy_dist.entropy().mean()
 
-        # Scale factor S (TODO: add exponential moving average for a more stable normalization)
-        flat_advantages = advantages.detach().view(-1)
+        # Scale factor S=EMA( Per(returns, 95) - Per(returns, 5) )
+        # where Per(x, p) is the p-th percentile of x (detaching to prevent gradient flow)
+        flat_returns = returns.detach().view(-1)
+        # Compute the 95th and 5th percentiles
+        quantile_95 = torch.quantile(flat_returns, 0.95)
+        quantile_5 = torch.quantile(flat_returns, 0.05)
         S = torch.clamp(
-            torch.quantile(flat_advantages, 0.95)
-            - torch.quantile(flat_advantages, 0.05),
+            quantile_95 - quantile_5,
             min=0.99,
         )
+        # TODO: EMA is the exponential moving average.
 
         actor_loss = (
             -((advantages.detach() / S) * log_probs).mean()
