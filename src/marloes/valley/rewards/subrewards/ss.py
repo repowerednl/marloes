478a0fcc240c40686960dfec01855161c86bd9f3
Lucas Velvis <lucasvelvis@gmail.com>
2025-05-08 16:06:22 +0200

    Add running mean to ss reward

--- src/marloes/valley/rewards/subrewards/ss.py
+++ src/marloes/valley/rewards/subrewards/ss.py
@@ -4,6 +4,7 @@ from marloes.results.extractor import Extractor
 from marloes.valley.rewards.subrewards.base import SubReward
 
 BUFFER = 2000
+ALPHA = 0.999  # running-mean decay
 
 
 class SSSubReward(SubReward):
@@ -12,6 +13,18 @@ class SSSubReward(SubReward):
     """
 
     name = "SS"
+
+    def __init__(
+        self,
+        config: dict,
+        active: bool = False,
+        scaling_factor: float = 1.0,
+    ):
+        """
+        Initializes the SSSubReward instance with activation and scaling properties.
+        """
+        super().__init__(config, active, scaling_factor)
+        self.r_mean = 0
 
     def calculate(
         self,
@@ -33,7 +46,11 @@ class SSSubReward(SubReward):
             # otherwise we are self-sufficient, so no penalty, but to not zero reward gradient (with flat reward)
             # every extra kw returns less of a reward
             if input_dict["net_grid_state"] > 0:
-                return -import_frac
+                # Calculate the running mean
+                raw_reward = -import_frac
+                self.r_mean = (ALPHA * self.r_mean) + (1 - ALPHA) * raw_reward
+                reward = raw_reward - self.r_mean
+                return reward
             else:
                 # Diminishing returns
                 surplus_kwh = abs(input_dict["net_grid_state"])
